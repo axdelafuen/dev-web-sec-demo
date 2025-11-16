e7a4befe9ae0a28bccbd0bd37759b32a713e8bfc
axdelafuen <axel.dlf@free.fr>
2025-11-16 13:50:05 +0100

    some tweaks

--- src/front/src/app/home/home.component.ts
+++ src/front/src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
